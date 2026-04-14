72f16b037dcb9bcba0633958c7ff8e75b87ca072
KICHA\KISHORE KUMAR <kishore@senstornet.com>
2026-04-14 14:06:00 +0530

    modify blog cardt

--- app/layout.tsx
+++ app/layout.tsx
@@ -239,6 +239,12 @@ const websiteSchema = {
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en" className={inter.variable}>
+      <head>
+        <link
+          href="https://fonts.googleapis.com/icon?family=Material+Icons"
+          rel="stylesheet"
+        />
+      </head>
       <body className="antialiased">
         {/* Remove extension-added attributes before hydration */}
         <Script
